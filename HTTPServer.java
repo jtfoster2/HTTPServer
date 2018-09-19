9e5e172fb31e5cafcd4ec615197b9d135fa4c8d4
Jtfoster2 <jtfoster2.phone@gmail.com>
2018-09-19 08:11:29 -0400

    Update HTTPServer.java

--- HTTPServer.java
+++ HTTPServer.java
@@ -6,11 +6,10 @@ import java.util.Scanner;
 
 public class HTTPServer {
     public static void main(String args[]) throws IOException{
-        int portnum;
         Scanner scan = new Scanner(System.in);
         System.out.println("What port would you like to open up on? Default: 8080");
-        scan.nextInt();
-        ServerSocket server = new ServerSocket(8080);
+        int portnum = scan.nextInt();
+        ServerSocket server = new ServerSocket(portnum);
         System.out.println("Listening for connection on port " + portnum);
         while(true){
             try(Socket socket = server.accept()){
